c9f0229168f3f9b913cc9f4abffa12e812b9fc83
Iyali Naomi Ake Mezeta <iyali.ake@blueoceantech.com.mx>
2019-12-26 10:51:05 -0600

    app

---
+++ AplicacionEventos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace AplicacionEventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"D:\AplicacionEventos\archivo.txt");
+
+            Console.WriteLine("contenido = \n");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+
+            Console.WriteLine("presione cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
